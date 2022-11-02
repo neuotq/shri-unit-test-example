8c15fc508484db76ee6b65ed0f300d1dd80088ad
Aleksandr Talalaev <neuotq@gmail.com>
2022-11-02 03:25:50 +0300

    feat: tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-export const testFunction = (a, b) => a + b;
+export const testFunction = (inn) => {
+    const innStr = String(inn)       
+    
+    if(innStr.length != 10 && innStr.length != 12 && !isIntegerString(innStr)) {        
+        return false
+    }
+    
+    if(innStr.length == 10) {
+        return checkTenInn(innStr)
+    }
+
+    return checkTwelveInn(innStr)
+};
+
+
+const isIntegerString = str => /^\+?\d+$/.test(str)
+
+const checkTenInn = innStr => {
+    const innArr = Array.from(innStr)    
+    const controlNum = parseInt(innArr.pop())
+    const weights = [2, 4, 10, 3, 5, 9, 4, 6, 8]
+    
+    const testNum = innArr.reduce(
+      (prevVal, curVal, currIndex) => prevVal + curVal * weights[currIndex],
+      0
+    ) % 11;
+
+    return testNum === controlNum || (controlNum === 0 && testNum > 9)
+}
+
+const checkTwelveInn = innStr => {
+    const innArr = Array.from(innStr)    
+    const controlNum1 = parseInt(innArr.pop())
+    const weights1 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    const testNum1 = innArr.reduce(
+      (prevVal, curVal, currIndex) => prevVal + curVal * weights1[currIndex],
+      0
+    ) % 11;
+
+    if(testNum1 !== controlNum1 || (controlNum1 !== 0 && testNum1 > 9)) {
+      return false
+    }
+    
+    const controlNum2 = parseInt(innArr.pop())
+    const weights2 = [7, 2,	4, 10, 3, 5, 9, 4, 6, 8]
+    
+
+    const testNum2 = innArr.reduce(
+        (prevVal, curVal, currIndex) => prevVal + curVal * weights2[currIndex],
+        0
+      ) % 11;
+
+    return testNum2 === controlNum2 || (controlNum2 === 0 && testNum2 > 9)
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,73 @@
 import {testFunction} from '.';
+const goodInn10 = [
+    6702838316,
+    6117254255,
+    2736366872,
+    7563045020,
+    '0913766388',
+    4838182483,
+    9107181170,
+    4815497818,
+    1017313194,
+    5370771751,
+    1088834411,
+    8621976791,
+    8180872644
+]
 
-describe('Тестовая функция, для наглядного примера', () => {
-    it('Складывает два числа вместе', () => {
-        expect(testFunction(2, 3)).toBe(5);
+const goodInn12 = [
+    471439872630,
+    518835114323,
+    104672257783,
+    544753898929,
+    578770001983,
+    686738171802,
+    206538749770,
+    338502122766,
+    474570151004,
+    141356890821,
+    674930910030,
+    642808931736,
+    436029480336
+]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+const getInn10 = () => goodInn10[getRandomInt(goodInn10.length)]
+const getInn10BadCRC = () => goodInn10[getRandomInt(goodInn10.length)] + getRandomInt(8)+1
+
+const getInn12 = () => goodInn12[getRandomInt(goodInn12.length)]
+const getInn12BadCRC = () => goodInn12[getRandomInt(goodInn12.length)] + getRandomInt(8)+1
+
+const getBadLongInn = () => Math.floor(getRandomInt(goodInn12.length) * (getRandomInt(10)+1) * 10)
+const getBadShortInn = () => Math.floor(getRandomInt(goodInn10.length) / (getRandomInt(10)+1) * 10)
+const getBadInnSymbol = () =>  'f' + String(getInn10).slice(1)
+
+describe('Проверяем функцию валидации ИНН', () => {
+    it('Проверяем верный 10 значный ИНН', () => {
+        expect(testFunction(getInn10())).toBeTruthy();
+    });    
+
+    it('Проверяем верный 12 значный ИНН', () => {
+        expect(testFunction(getInn12())).toBeTruthy();
+    });
+
+    it('Проверяем не верный 10 значный ИНН по контрольной сумме', () => {
+        expect(testFunction(getInn10BadCRC())).toBeFalsy();
+    });
+
+    it('Проверяем не верный 12 значный ИНН по контрольной сумме', () => {
+        expect(testFunction(getInn12BadCRC())).toBeFalsy();
+    });
+
+    it('Проверяем не верный ИНН по длине', () => {
+        expect(testFunction(getBadLongInn())).toBeFalsy();
+        expect(testFunction(getBadShortInn())).toBeFalsy();
+    });
+
+    it('Проверяем не верный ИНН по символам', () => {
+        expect(testFunction(getBadInnSymbol())).toBeFalsy();        
     });
 });
